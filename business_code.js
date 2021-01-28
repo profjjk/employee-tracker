2cfb3bc7b2aba35c79bf9f2b76a3678f097c5a83
Jordan Kelly <profjjk@gmail.com>
2021-01-27 18:02:35 -0800

    add main menu prompts

--- business_code.js
+++ business_code.js
@@ -6,6 +6,7 @@ const inquirer = require('inquirer');
 
 // CONNECT TO LOCAL DATABASE
 // ============================================================
+
 // Configure connection.
 const connection = mysql.createConnection({
   host: "localhost",
@@ -15,7 +16,7 @@ const connection = mysql.createConnection({
   database: "business_db"
 })
 
-// Start connection.
+// Start connection & begin prompts.
 connection.connect(function(err) {
   if (err) throw err;
   mainMenu();
@@ -24,3 +25,25 @@ connection.connect(function(err) {
 
 // PROMPTS & QUERIES
 // ============================================================
+
+// Main menu prompts.
+function mainMenu() {
+  inquirer.prompt({
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: [
+      "1) ADD department",
+      "2) ADD role",
+      "3) ADD employee",
+      "4) VIEW departments",
+      "5) VIEW roles",
+      "6) VIEW employees",
+      "7) VIEW employees by manager",
+      "8) UPDATE employee managers",
+      "9) DELETE department",
+      "10) DELETE role",
+      "11) DELETE employee",
+    ]
+  })
+}
