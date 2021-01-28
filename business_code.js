68dff0cef57ba088536fae03861b8ff35a3f4d0d
Jordan Kelly <profjjk@gmail.com>
2021-01-27 17:39:41 -0800

    connect to local database

--- business_code.js
+++ business_code.js
@@ -1,2 +1,15 @@
+// DEPENDENCIES
+// ============================================================
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+
+// CONNECT TO LOCAL DATABASE
+// ============================================================
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "business_db"
+})
